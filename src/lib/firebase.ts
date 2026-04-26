38b26a09a90cc45f9e23b99f8d302e526a24faf7
MattCowBoy <nobelmuzik@gmail.com>
2026-04-26 23:46:53 +0200

    debug: log firebase config presence

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,6 +12,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log('[Firebase] apiKey defined?', !!firebaseConfig.apiKey, '| projectId:', firebaseConfig.projectId);
 const app = initializeApp(firebaseConfig);
 
 export const firebaseAuth = getAuth(app);
